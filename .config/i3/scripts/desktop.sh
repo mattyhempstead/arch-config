79a5756b179cd811e6ee362b0496020b173b82aa
= <=>
2020-06-15 03:20:16 +1000

    This is what happens when you rearrange your display cables

--- .config/i3/scripts/desktop.sh
+++ .config/i3/scripts/desktop.sh
@@ -1,9 +1,13 @@
 ## Assorted commands for configuring desktop
 
 # Monitor placement
-xrandr --output HDMI-3 --primary
-xrandr --output DP-1 --right-of HDMI-3
-xrandr --output DVI-I-1 --left-of HDMI-3 
+#xrandr --output HDMI-3 --primary
+#xrandr --output DP-1 --right-of HDMI-3
+#xrandr --output DP-3 --left-of HDMI-3 
+xrandr --output DP-5 --primary
+xrandr --output DP-1 --right-of DP-5
+xrandr --output HDMI-0 --left-of DP-5
+
 
 # Mouse sensitivity
 xinput set-prop 'Logitech G502 HERO Gaming Mouse' 'libinput Accel Speed' -0.75
