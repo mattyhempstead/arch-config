2edaf1f37856f4689a0a52b0447ec30ecfc3e11e
mattyhempstead <matty.hempstead@gmail.com>
2021-07-10 12:14:57 +1000

    Fix white oversaturation with xrandr brightness

--- .config/i3/scripts/desktop.sh
+++ .config/i3/scripts/desktop.sh
@@ -1,14 +1,12 @@
 ## Assorted commands for configuring desktop
 
 # Monitor placement
-xrandr --output DP-1 --primary
-xrandr --output DP-5 --left-of DP-1
-
-xrandr --output HDMI-0 --right-of DP-1
-xrandr --output HDMI-0 --mode 1600x900 
+xrandr --output DP-1 --primary --mode 1920x1080 --rate 60 --brightness 0.9
+xrandr --output DP-5 --left-of DP-1 --mode 1920x1080 --rate 60
+xrandr --output HDMI-0 --right-of DP-1 --mode 1920x1080 --rate 144
 
 # Mouse sensitivity
-xinput set-prop 'Logitech G502 HERO Gaming Mouse' 'libinput Accel Speed' -0.75
+#xinput set-prop 'Logitech G502 HERO Gaming Mouse' 'libinput Accel Speed' -0.75
 
 xinput set-prop 'pointer:Logitech MX Master 3' 'libinput Accel Speed' -0.8
 
